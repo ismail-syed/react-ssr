f782b71afa7d9453753f330997fa59d8e05253dc
Chun <18782929253@163.com>
2018-03-22 20:56:16 +0800

    创建Comment相关组件

---
+++ src/comment/CommentApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import CommentInput from './commentComponent/CommentInput';
+import CommentList from './commentComponent/CommentList';
+
+export default class CommentApp extends Component{
+    render () {
+        return (
+            <div>
+                <CommentInput />
+                <CommentList />
+            </div>
+        )
+    }
+}
---
+++ src/comment/commentComponent/Comment.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Comment extends Component{
+    render () {
+        return (
+            <div></div>
+        )
+    }
+}
