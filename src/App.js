72e10888a8a228c8e92332e17119ba7524972b2e
Chun <18782929253@163.com>
2018-03-24 23:54:39 +0800

    安装redux react-redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import CommentApp from './comment/CommentApp';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CommentApp />
+
       </div>
     );
   }
