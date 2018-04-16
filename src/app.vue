0f674dfc2fe544a8e20d4c9119f76f12dabf93c0
Chun <18782929253@163.com>
2018-04-17 00:26:32 +0800

    🎉  initial commit

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="test">{{ text }}</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      text: 'Hello vue'
+    }
+  }
+}
+</script>
+
+<style>
+.test{
+  color: red;
+}
+</style>
