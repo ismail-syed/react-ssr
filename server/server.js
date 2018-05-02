b2f915a2740830848ed8a7dad20ebdb922aa630a
Chun <18782929253@163.com>
2018-05-03 00:20:59 +0800

    ⬆️ 服务端渲染基础路由

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 const favicon = require('serve-favicon');
 const app = express();
 
 // 是否时开发环境
 const isDev = process.env.NODE_ENV === 'development';
-console.log(isDev);
+
+app.use(bodyParser.json()); // 请求放入req.body中
+app.use(bodyParser.urlencoded({
+  extended: false // 表单请求也放入req.body中
+}));
+app.use(session({
+  maxAge: 10*60*100,
+  name: 'access_id',
+  resave: false,
+  saveUninitialized: false,
+  secret: 'Zsmart-TF_Bank'
+}));
 
 app.use(favicon(path.join(__dirname, '../favicon.ico')));
 
--- server/serverEntry.js
+++ server/serverEntry.js
@@ -1,4 +1,17 @@
 import React from 'react';
+import { StaticRouter } from 'react-router-dom';
+import { Provider, useStaticRendering } from 'mobx-react';
 import App from '../client/views/App';
 
-export default <App />;
+// 防止mobx在服务端渲染时发生数据变化
+useStaticRendering(true);
+
+export default (stores,routerContext,url) => {
+  return (
+    <Provider {...stores}>
+      <StaticRouter context={routerContext} location={url}>
+        <App />
+      </StaticRouter>
+    </Provider>
+  )
+};
